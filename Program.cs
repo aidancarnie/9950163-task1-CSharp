36d9e566c6b46a7dfdde7107a42446584a1f40ee
Aidan Carnie <9950163@BCS.NET.NZ>
2017-08-14 11:21:44 +1200

    Second Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace _9950163_task1_CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double Celcius = 0;
+            double Fahrenheit = 0;
+
+            Console.WriteLine("This program will convert Celcius to Fahrenheit");
+            
+            Console.WriteLine("\r\nEnter either Celcius or Fahrenheit: ");
+            string userChoice = Console.ReadLine();
+
+            if(userChoice == "Celcius")
+            {
+                Console.WriteLine("Enter the amount of Celcius you want to convert");
+                Celcius = double.Parse(Console.ReadLine());
+                Celcius = Fahrenheit - 32 * 5 / 9;
+                Console.WriteLine("That in Fahrenheit is: ", Celcius);
+            }
+            else if(userChoice == "Fahrenheit")
+            {
+                Console.WriteLine("Enter the amount of Fahrenheit you want to convert");
+                Fahrenheit = double.Parse(Console.ReadLine());
+                Fahrenheit = Celcius * 9 / 5 + 32;
+                Console.WriteLine("That in Celsius is: ", Fahrenheit);
+            }
+            Console.ReadLine();
         }
     }
 }
