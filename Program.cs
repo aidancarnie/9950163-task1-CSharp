0cbf499f4c6d09f69514ad88a2f5053729e8a85b
Aidan Carnie <9950163@bcs.net.nz>
2017-08-14 09:58:58 +1200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _9950163_task1_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
